28ea1a9b1e9e2f0e05f6bdf937295a3815a4a5f7
Alysson Pereira <alysson.nascimento2111@gmail.com>
2024-01-29 18:44:05 -0300

    Melhoramento na apresentação dos Estados

--- src/main/java/br/com/example/controller/EstadoController.java
+++ src/main/java/br/com/example/controller/EstadoController.java
@@ -4,15 +4,18 @@
  */
 package br.com.example.controller;
 
+import br.com.example.model.EstadoDTO;
 import br.com.example.model.EstadoView;
 import br.com.example.repository.EstadoRepository;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -27,20 +30,68 @@ public class EstadoController {
     @Autowired
     private EstadoRepository estadoRepository;
     
-    @GetMapping("/estados")
-    public List<EstadoView> listarEstados(){
-        return estadoRepository.listarEstados();
+    private EstadoDTO estadoViewTable(EstadoView estadoView) {
+        return new EstadoDTO(
+                estadoView.getCodigo(),
+                estadoView.getNome(),
+                estadoView.getSigla());
     }
+    private EstadoDTO estadoViewMap(EstadoView estadoView) {
+        return new EstadoDTO(
+                estadoView.getCodigo(),
+                estadoView.getNome(),
+                estadoView.getSigla(),
+                estadoView.getGeometria());
+    }
+    
+    
+    @GetMapping("/estados")
+    public List<EstadoDTO> listarEstados(
+            @RequestParam(value = "visibility", defaultValue = "map") String visibility){
+        
+        List<EstadoDTO> response = new ArrayList<>();
+        if(visibility.equals("table")) {
+            estadoRepository.listarEstados()
+                    .forEach(estado -> response.add(estadoViewTable(estado)));
+        } else {
+             estadoRepository.listarEstados()
+                    .forEach(estado -> response.add(estadoViewMap(estado)));
+        }
+        return response;
+    }
+    
     
     @GetMapping("/estados_regiao/{regiao}")
-    public List<EstadoView> listarEstadosRegiao(@PathVariable String regiao) {
-        return estadoRepository.listarEstadosRegiao(regiao);
+    public List<EstadoDTO> listarEstadosRegiao(
+            @PathVariable String regiao,
+            @RequestParam(value = "visibility", defaultValue = "map") String visibility) {
+        List<EstadoDTO> response = new ArrayList<>();
+        if(visibility.equals("table")) {
+            estadoRepository.listarEstadosRegiao(regiao)
+                    .forEach(estado -> response.add(estadoViewTable(estado)));
+        } else {
+             estadoRepository.listarEstadosRegiao(regiao)
+                    .forEach(estado -> response.add(estadoViewMap(estado)));
+        }
+        return response;
     }
     
+    
     @GetMapping("/estados_vizinhos/{uf}")
-    public List<EstadoView> estadosVizinhos(@PathVariable String uf) {
-        return estadoRepository.estadosVizinhos(uf);
+    public List<EstadoDTO> estadosVizinhos(
+            @PathVariable String uf,
+            @RequestParam(value = "visibility", defaultValue = "map") String visibility) {
+        List<EstadoDTO> response = new ArrayList<>();
+        if(visibility.equals("table")) {
+            estadoRepository.estadosVizinhos(uf)
+                    .forEach(estado -> response.add(estadoViewTable(estado)));
+        } else {
+             estadoRepository.estadosVizinhos(uf)
+                    .forEach(estado -> response.add(estadoViewMap(estado)));
+        }
+        return response;
     }
+    
     
     @GetMapping("/distancia_estados/{ufA}/{ufB}")
     public Double distanciaEstados(@PathVariable String ufA, @PathVariable String ufB) {
--- src/main/java/br/com/example/repository/EstadoRepository.java
+++ src/main/java/br/com/example/repository/EstadoRepository.java
@@ -17,18 +17,18 @@ import org.springframework.data.jpa.repository.Query;
 public interface EstadoRepository extends JpaRepository<Estado, Integer>{
     
     //@Query(value = "select new br.com.example.model.EstadoView(e.codigo, e.nome, e.sigla, e.geometria) from Estado e")
-    @Query(value = "select new br.com.example.model.EstadoView(e.codigo, e.nome, e.sigla, e.geometria) from Estado e")
+    @Query(value = "select new br.com.example.model.EstadoView(e.codigo, e.nome, e.sigla, e.geometria) from Estado e ORDER BY e.nome")
     List<EstadoView> listarEstados();
     
     
     //@Query(value = "select new br.com.example.model.EstadoView(e.codigo, e.nome, e.sigla, e.geometria) from Estado e where e.nomeRegiao = :regiao")
-    @Query(value = "select new br.com.example.model.EstadoView(e.codigo, e.nome, e.sigla, e.geometria) from Estado e where e.nomeRegiao = :regiao")
+    @Query(value = "select new br.com.example.model.EstadoView(e.codigo, e.nome, e.sigla, e.geometria) from Estado e where e.nomeRegiao = :regiao ORDER BY e.nome")
     List<EstadoView> listarEstadosRegiao(String regiao);
     
     
     @Query(value = "SELECT new br.com.example.model.EstadoView(e2.codigo, e2.nome, e2.sigla, e2.geometria) FROM Estado e1\n" +
                     "INNER JOIN Estado e2 ON touches(e1.geometria, e2.geometria) = true\n" +
-                    "WHERE e1.sigla = :ufEstado")
+                    "WHERE e1.sigla = :ufEstado ORDER BY e2.nome")
     List<EstadoView> estadosVizinhos(String ufEstado);
     
     
